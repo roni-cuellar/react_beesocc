f53e6ee2ed7a2de41d6d36dfb29f8594347de358
Jorge Alzate <jorgealzatejg@gmail.com>
2026-05-26 11:10:50 -0500

    cambio navegador

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
 import Footer from "./components/Footer/Footer"
-import { BrowserRouter } from "react-router-dom"
+
 import "./App.css"
 
 import AppRoutes from "./routes/AppRoutes"
@@ -8,13 +9,13 @@ import AppRoutes from "./routes/AppRoutes"
 function App() {
 
   return (
-  <BrowserRouter>
+  <>
       <Navbar />
 
     <AppRoutes />
 
       <Footer />
-  </BrowserRouter>
+  </>
   )
 }
 
